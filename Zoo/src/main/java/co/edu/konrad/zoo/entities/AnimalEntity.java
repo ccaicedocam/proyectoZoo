aac0967318587540cf4e1b986633b50eaabb1465
jaleman1005 <jorgealeman31@gmail.com>
2019-03-09 18:20:16 -0500

    se crean las entidades de animal, comida animal y comida

--- Zoo/src/main/java/co/edu/konrad/zoo/entities/AnimalEntity.java
+++ Zoo/src/main/java/co/edu/konrad/zoo/entities/AnimalEntity.java
@@ -23,11 +23,66 @@ public class AnimalEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_animal")
     private Long idAnimal;
+    
+    @Column (name = "nom_Animal")
+    private String nomAnimal;
+    
+    @Column (name = "nom_cientifico")
+    private String nomCienfitico;
+    
+    @Column (name = "especie")
+    private String especia;
+    
+    @Column (name = "Tipo_animal")
+    private String tipoAnimal;
+    
+    @Column (name = "descripcion")
+    private String descripcion;
 
     /**
      * Constructor
      */
     public AnimalEntity() {
+    }
+    
+    public String getNomAnimal() {
+        return nomAnimal;
+    }
+
+    public void setNomAnimal(String nomAnimal) {
+        this.nomAnimal = nomAnimal;
+    }
+
+    public String getNomCienfitico() {
+        return nomCienfitico;
+    }
+
+    public void setNomCienfitico(String nomCienfitico) {
+        this.nomCienfitico = nomCienfitico;
+    }
+
+    public String getEspecia() {
+        return especia;
+    }
+
+    public void setEspecia(String especia) {
+        this.especia = especia;
+    }
+
+    public String getTipoAnimal() {
+        return tipoAnimal;
+    }
+
+    public void setTipoAnimal(String tipoAnimal) {
+        this.tipoAnimal = tipoAnimal;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
     
     public Long getIdAnimal() {

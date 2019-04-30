030314d5ae3ef28bc1ce8444f9516013f4b07856
cristec85 <40065019+cristec85@users.noreply.github.com>
2019-04-29 23:12:03 -0500

    Entity horarios y lugar

--- Zoo/src/main/java/co/edu/konrad/zoo/entities/horarioShowsEntity.java
+++ Zoo/src/main/java/co/edu/konrad/zoo/entities/horarioShowsEntity.java
@@ -16,6 +16,56 @@ import javax.persistence.Id;
  *
  * @author cristian_caicedo
  */
-public class horarioShowsEntity {
+@Entity (name = "horarioShows")
+public class horarioShowsEntity implements Serializable {
     
+        @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_Hora_Shows")
+    private Long idHoraShows;
+
+    @Column (name = "disponibilidad")
+    private String disponibilidad;
+
+    @Column (name = "id_Lugar")
+    private String idLugar;
+
+    @Column (name = "id_Show")
+    private String idShow;
+
+    public horarioShowsEntity() {
+    
+    }
+
+    public Long getIdHoraShows() {
+        return idHoraShows;
+    }
+
+    public void setIdHoraShows(Long idHoraShows) {
+        this.idHoraShows = idHoraShows;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public String getIdLugar() {
+        return idLugar;
+    }
+
+    public void setIdLugar(String idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    public String getIdShow() {
+        return idShow;
+    }
+
+    public void setIdShow(String idShow) {
+        this.idShow = idShow;
+    }
 }
--- Zoo/src/main/java/co/edu/konrad/zoo/entities/lugarEntity.java
+++ Zoo/src/main/java/co/edu/konrad/zoo/entities/lugarEntity.java
@@ -4,11 +4,43 @@
  * and open the template in the editor.
  */
 package co.edu.konrad.zoo.entities;
-
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 /**
  *
  * @author cristian_caicedo
  */
-public class lugarEntity {
+@Entity (name = "lugar")
+public class lugarEntity implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_Lugar")
+    private Long idLugar;
+
+    @Column (name = "nombre")
+    private String nombre;  
+
+    public lugarEntity() {
+    }
+
+    public Long getIdLugar() {
+        return idLugar;
+    }
+
+    public void setIdLugar(Long idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }

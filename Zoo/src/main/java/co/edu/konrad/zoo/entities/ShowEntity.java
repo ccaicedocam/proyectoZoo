9a4e1efc41ada29bcf16ff63d32fc5ac017bb6b3
cristian caicedo <cristian_85@outlook.com>
2019-06-01 08:29:38 -0500

    Update ShowEntity.java

--- Zoo/src/main/java/co/edu/konrad/zoo/entities/ShowEntity.java
+++ Zoo/src/main/java/co/edu/konrad/zoo/entities/ShowEntity.java
@@ -6,29 +6,74 @@
 package co.edu.konrad.zoo.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
 
 /**
  *
  * @author jorge
  */
-@Entity (name = "Show")
+//@Entity (name = "Show")
+@Entity
 public class ShowEntity implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_show")
     private Long idShow;
+    
+    @Column (name = "Idnombre")
+    private String Idnombre;
+    
+    @Column (name = "Idhora")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date Idhora;
+    
+    @ManyToOne
+    @JoinColumn(name ="id_cod_empleado")
+    private empleadoEntity idCodEmpleado;
 
     /**
      * Constructor
      */
     public ShowEntity() {
+        
     }
+
+    public String getIdnombre() {
+        return Idnombre;
+    }
+
+    public void setIdnombre(String Idnombre) {
+        this.Idnombre = Idnombre;
+    }
+
+    public Date getIdhora() {
+        return Idhora;
+    }
+
+    public void setIdhora(Date Idhora) {
+        this.Idhora = Idhora;
+    }
+
+    public empleadoEntity getIdCodEmpleado() {
+        return idCodEmpleado;
+    }
+
+    public void setIdCodEmpleado(empleadoEntity idCodEmpleado) {
+        this.idCodEmpleado = idCodEmpleado;
+    }
+
+
+
+
 
     public Long getIdShow() {
         return idShow;
